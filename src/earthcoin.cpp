ed41bd93b213c62ad603bf27e51fdd95e7290189
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2021-02-20 09:12:46 +0100

    Update earthcoin.cpp
    Earthcoin block reward scheme

--- src/earthcoin.cpp
+++ src/earthcoin.cpp
@@ -22,6 +22,9 @@ int static generateMTRandom(unsigned int s, int range)
 // a retarget, so we need to handle minimum difficulty on all blocks.
 bool AllowDigishieldMinDifficultyForBlock(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
+    // never allow minimal difficulty, EAC retarget works fine	
+    return false;
+/*    
     // check if the chain allows minimum difficulty blocks
     if (!params.fPowAllowMinDifficultyBlocks)
         return false;
@@ -32,11 +35,14 @@ bool AllowDigishieldMinDifficultyForBlock(const CBlockIndex* pindexLast, const C
         return false;
 
     // Allow for a minimum block time if the elapsed time > 2*nTargetSpacing
-    return (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2);
+    return (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2);*/
 }
 
 unsigned int CalculateEarthcoinNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
 {
+    // do not change retarget algo	
+    return CalculateNextWorkRequired(pindexLast, nFirstBlockTime, params);
+   /* 
     int nHeight = pindexLast->nHeight + 1;
     bool fNewDifficultyProtocol = (nHeight >= 145000);
     // bool fNewDifficultyProtocol = (nHeight >= params.GetDigiShieldForkBlock());
@@ -92,6 +98,7 @@ unsigned int CalculateEarthcoinNextWorkRequired(const CBlockIndex* pindexLast, i
     LogPrintf("After:  %08x  %s\n", bnNew.GetCompact(), bnNew.ToString());
 
     return bnNew.GetCompact();
+    */
 }
 
 bool CheckAuxPowProofOfWork(const CBlockHeader& block, const Consensus::Params& params)
@@ -134,24 +141,64 @@ bool CheckAuxPowProofOfWork(const CBlockHeader& block, const Consensus::Params& 
 
 CAmount GetEarthcoinBlockSubsidy(int nHeight, const Consensus::Params& consensusParams, uint256 prevHash)
 {
-    int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
+    const CAmount nMinSubsidy = 1 * COIN;
+    // base payout
+    CAmount nSubsidy = 10000 * COIN;
 
-    if (nHeight < 145000) // && !consensusParams.SimplifiedRewards())
-    {
-        // Old-style rewards derived from the previous block hash
-        const std::string cseed_str = prevHash.ToString().substr(7, 7);
-        const char* cseed = cseed_str.c_str();
-        char* endp = NULL;
-        long seed = strtol(cseed, &endp, 16);
-        CAmount maxReward = (1000000 >> halvings) - 1;
-        int rand = generateMTRandom(seed, maxReward);
-
-        return (1 + rand) * COIN;
-    } else if (nHeight < (6 * consensusParams.nSubsidyHalvingInterval)) {
-        // New-style constant rewards for each halving interval
-        return (500000 * COIN) >> halvings;
-    } else {
-        // Constant inflation
-        return 10000 * COIN;
+    // first block receives 2% premine to support EarthCoin
+    if (nHeight == 1) {
+	    nSubsidy = 270000000LL * COIN;
+	    return nSubsidy;
     }
+
+    // relative height inside main period (approx. 1 year)
+    int nHeightM = nHeight % 525600;
+    double phase = ((double)nHeightM) / 525600.0 * 2.0 * M_PI;
+    // modify base payout with seasonal effect
+    nSubsidy += ((int)(2000.0 * sin(phase))) * COIN;
+
+    // bonus zones
+
+    // get number of days since the inception of EarthCoin
+    int day = nHeight / 1440 + 1;
+
+    // regular bonus zones
+
+    // every 31 days, payout is increased by a factor of 5
+    if (day % 31 == 0) {
+	    nSubsidy *= 5;
+    }
+    // every 14 days, payout is increased by a factor of 2
+    else if (day % 14 == 0) {
+	    nSubsidy *= 2;
+    }
+
+    // special bonus zones
+
+    // the first three days were special (approx. 12/21-21/24 in the year of 2013)
+    switch (day) {
+        // 5 times the normal payout on day 1
+	case 1:
+	    nSubsidy *= 5;
+	    break;
+	// 3 times the normal payout on day 2
+	case 2:
+	    nSubsidy *= 3;
+	    break;
+	// 2 times the normal payout on day 3
+	case 3:
+	    nSubsidy *= 2;
+	    break;
+    }
+
+    // subsidy is cut in half every 525600 blocks,
+    // which will occur approximately every 12 months
+    nSubsidy >>= (nHeight / 525600);
+
+    // nevertheless, there will a minimum payout of 1 EarthCoin
+    if (nSubsidy < nMinSubsidy) {
+	    nSubsidy = nMinSubsidy;
+    }
+
+    return nSubsidy;
 }
