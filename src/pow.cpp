be692515c0d83318593de7196f9483a0f9249451
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2021-02-20 09:20:08 +0100

    Update pow.cpp
    Earthcoin retarget algo

--- src/pow.cpp
+++ src/pow.cpp
@@ -17,79 +17,39 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
 {
     assert(pindexLast != nullptr);
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
-
-    // Earthcoin: Special rules for minimum difficulty blocks with Digishield
-    if (AllowDigishieldMinDifficultyForBlock(pindexLast, pblock, params))
-    {
-        // Special difficulty rule for testnet:
-        // If the new block's timestamp is more than 2* nTargetSpacing minutes
-        // then allow mining of a min-difficulty block.
+    
+    // Genesis block
+    if (pindexLast == NULL)
         return nProofOfWorkLimit;
-    }
-
-    // Only change once per difficulty adjustment interval
-    bool fNewDifficultyProtocol = (pindexLast->nHeight >= 145000);
-    const int64_t difficultyAdjustmentInterval = fNewDifficultyProtocol
-                                                 ? 1
-                                                 : params.DifficultyAdjustmentInterval();
-    if ((pindexLast->nHeight+1) % difficultyAdjustmentInterval != 0)
-    {
-        if (params.fPowAllowMinDifficultyBlocks)
-        {
-            // Special difficulty rule for testnet:
-            // If the new block's timestamp is more than 2* 10 minutes
-            // then allow mining of a min-difficulty block.
-            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
-                return nProofOfWorkLimit;
-            else
-            {
-                // Return the last non-special-min-difficulty-rules-block
-                const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
-                    pindex = pindex->pprev;
-                return pindex->nBits;
-            }
-        }
-        return pindexLast->nBits;
-    }
-
-    // Litecoin: This fixes an issue where a 51% attack can change difficulty at will.
-    // Go back the full period unless it's the first retarget after genesis. Code courtesy of Art Forz
-    int blockstogoback = difficultyAdjustmentInterval-1;
-    if ((pindexLast->nHeight+1) != difficultyAdjustmentInterval)
-        blockstogoback = difficultyAdjustmentInterval;
-
-    // Go back by what we want to be 14 days worth of blocks
-    int nHeightFirst = pindexLast->nHeight - blockstogoback;
-    assert(nHeightFirst >= 0);
-    const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
-    assert(pindexFirst);
-
-    return CalculateEarthcoinNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
+    // 1st block
+    if (pindexLast->pprev == NULL)
+	    return nProofOfWorkLimit;
+    // 2nd block
+    if (pindexLast->pprev->pprev == NULL)
+	    return nProofOfWorkLimit;
+    const CBlockIndex* pindexFirst = pindexLast->pprev;
+    return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
 }
 
 unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
 {
     if (params.fPowNoRetargeting)
         return pindexLast->nBits;
-
-    // Limit adjustment step
-    int64_t nActualTimespan = pindexLast->GetBlockTime() - nFirstBlockTime;
-    if (nActualTimespan < params.nPowTargetTimespan/4)
-        nActualTimespan = params.nPowTargetTimespan/4;
-    if (nActualTimespan > params.nPowTargetTimespan*4)
-        nActualTimespan = params.nPowTargetTimespan*4;
-
+    int64_t nActualSpacing = pindexLast->GetBlockTime() - nFirstBlockTime;
+    // limit the adjustment
+    if (nActualSpacing < params.nPowTargetSpacing/16)
+	    nActualSpacing = params.nPowTargetSpacing/16;
+    if (nActualSpacing > params.nPowTargetSpacing*16)
+	    nActualSpacing = params.nPowTargetSpacing*16;
     // Retarget
-    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
     arith_uint256 bnNew;
     bnNew.SetCompact(pindexLast->nBits);
-    bnNew *= nActualTimespan;
-    bnNew /= params.nPowTargetTimespan;
-
-    if (bnNew > bnPowLimit)
+    const int64_t nInterval = 30;			// nTargetTimespan / nTargetSpacing = 30*60 / 60 = 30
+    bnNew *= ((nInterval - 1) * params.nPowTargetSpacing + 2 * nActualSpacing);
+    bnNew /= ((nInterval + 1) * params.nPowTargetSpacing);
+    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
+    if (bnNew > bnPowLimit )
         bnNew = bnPowLimit;
-
     return bnNew.GetCompact();
 }
 
