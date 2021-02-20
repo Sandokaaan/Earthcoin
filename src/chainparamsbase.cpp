30f62833acf6a3280035d706b488d5f4cc2855de
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2021-02-20 08:54:50 +0100

    Update chainparamsbase.cpp
    Earthcoin default ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -33,11 +33,11 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 22555);
+        return MakeUnique<CBaseChainParams>("", 15678);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet3", 44555);
+        return MakeUnique<CBaseChainParams>("testnet4", 25678);
     else if (chain == CBaseChainParams::REGTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 66555);
+        return MakeUnique<CBaseChainParams>("regtest", 45678);
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
