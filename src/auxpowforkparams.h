aa597ff43caaebbfe30dbfc6bb6f316b34b3770c
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2021-02-22 17:06:39 +0100

    Move AUXPOW fork params to a separate file
    They are needed at more places in the code.

---
+++ src/auxpowforkparams.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 The Earthcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef EARTHCOIN_AUXPOW_FORK_PARAMS_H
+#define EARTHCOIN_AUXPOW_FORK_PARAMS_H
+
+const int AUXPOW_CHAIN_ID = 0x205d;         // 0x2000 | 0x005d = v2.0.x blockID + EAC_key_Prefix!
+const int AUXPOW_START_HEIGHT = 3500000;    // May 2021 ?
+const int REGULAR_POW_END_HEIGHT = -1;      // Never ?
+
+#endif // EARTHCOIN_AUXPOW_FORK_PARAMS_H
