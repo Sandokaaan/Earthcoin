3d919699aefdc3629680f6cb2dc39db506b0586c
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2021-02-22 18:20:13 +0100

    Removed unused SCRIPT_ADDRESS prefixes
    They were from original BTC source, not used here

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -144,8 +144,8 @@ public:
 	vSeeds.emplace_back("dnsseed.eacpay.com");    
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,93);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
-        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,33);  // segwit addresses will start with E
+        // base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);  // 
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,33);    // segwit addresses will start with E
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,221);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
@@ -311,8 +311,8 @@ public:
 	vSeeds.emplace_back("testnet-seed.deveac.com");    
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
-        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,58);
+        //base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,58);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
@@ -413,8 +413,8 @@ public:
         };
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
-        base58Prefixes[SCRIPT_ADDRESS2] = std::vector<unsigned char>(1,58);
+        // base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,58);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x35, 0x83, 0x94};
