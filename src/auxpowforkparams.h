561249d9351639348c3206197b3ef970a961803f
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2021-02-23 17:44:21 +0100

    AUXPOW_CHAIN_ID set back to 0x205d
    To be consistent with previous block version 0x20000000

--- src/auxpowforkparams.h
+++ src/auxpowforkparams.h
@@ -5,7 +5,7 @@
 #ifndef EARTHCOIN_AUXPOW_FORK_PARAMS_H
 #define EARTHCOIN_AUXPOW_FORK_PARAMS_H
 
-const int AUXPOW_CHAIN_ID = 0x5d;           // EAC_key_Prefix
+const int AUXPOW_CHAIN_ID = 0x205d;         // To be consistent with previous block version 0x20000000
 const int AUXPOW_START_HEIGHT = 3500000;    // May 2021 ?
 const int REGULAR_POW_END_HEIGHT = -1;      // Never ?
 
