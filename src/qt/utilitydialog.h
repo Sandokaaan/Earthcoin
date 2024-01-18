e6fcf785d72a5302edfe786df3a4acdfc4fbec7d
Sandokaaan <30953425+Sandokaaan@users.noreply.github.com>
2024-01-18 23:05:52 +0100

    Update utilitydialog.h
    Fix a compile warning

--- src/qt/utilitydialog.h
+++ src/qt/utilitydialog.h
@@ -45,7 +45,7 @@ class ShutdownWindow : public QWidget
     Q_OBJECT
 
 public:
-    explicit ShutdownWindow(QWidget *parent=0, Qt::WindowFlags f=0);
+    explicit ShutdownWindow(QWidget *parent=nullptr, Qt::WindowFlags f=Qt::WindowFlags());  // Sando: Fix a compile warning
     static QWidget *showShutdownWindow(EarthcoinGUI *window);
 
 protected:
