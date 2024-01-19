093f4dd6da971ed121141808360360eb7b2d9cfb
Sandokaaan <geo@vosar.cz>
2024-01-19 15:56:57 +0100

    A script for compile with QT-wallet

--- buildgui.sh
+++ buildgui.sh
@@ -1,1 +1,14 @@
-#
+#!/bin/sh
+# Copyright (c) 2024 The Earthcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+./autogen.sh
+./configure --with-incompatible-bdb --with-gui
+make
+cd src
+strip earthcoind
+strip earthcoin-cli
+strip earthcoin-tx
+cd qt
+# strip earthcoin.gui
